08fab1ddc7110ea9a8cce025e72e7035cfd11b74
DalexisValencia <35255633+DalexisValencia@users.noreply.github.com>
2018-09-24 08:28:05 -0500

    solucionando error #4
    Añadiendo start de la app.
    Solucionando errores de comillas

--- basic.js
+++ basic.js
@@ -1,4 +1,10 @@
 var el = document.getElementById("div");
-el.addEventListener("click", function(){
-  console.warn("Esta función aparace al final!);
+el.addEventListener("click", function(event, docu){
+  console.warn("Esta función aparace al final!");
+  if ( docu == "init"){
+    console.warn("lanzamos la app");
+  }
+  else{
+    console.info("Nos mantenemos en espera...");
+  }
 });
